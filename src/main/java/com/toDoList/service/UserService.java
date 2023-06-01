90214d850017355126b5f6ab19a6c35f0112984c
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-06-01 22:05:52 +0900

    feat: 회원가입 과정 구현

--- src/main/java/com/toDoList/service/UserService.java
+++ src/main/java/com/toDoList/service/UserService.java
@@ -1,11 +1,17 @@
 package com.toDoList.service;
 
 import com.toDoList.domain.User;
+import com.toDoList.dto.UserDto;
+import com.toDoList.dto.UserDto.SignUpRequest;
+import com.toDoList.global.config.email.EmailService;
 import com.toDoList.repository.springDataJpa.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -13,9 +19,37 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserService {
     private final UserRepository userRepository;
+    private final EmailService emailService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public boolean checkEmailDup(String email) {
+        Optional<User> isDup = userRepository.findByEmail(email);
+        if (isDup.isPresent())
+            return false;
+        return true;
+    }
+
+    public String sendEmail(String email) throws Exception {
+        String key = emailService.sendSimpleMessage(email);
+        return key;
+    }
 
     public User validateUserEmail(String email) {
         User user = userRepository.findByEmail(email).orElseThrow();
         return user;
+    }
+
+    public boolean checkNickDup(String nickName) {
+        Optional<User> isDup = userRepository.findByNickName(nickName);
+        if (isDup.isPresent())
+            return false;
+        return true;
+    }
+
+    public void signUp(SignUpRequest signUpRequest) {
+        String password = bCryptPasswordEncoder.encode(signUpRequest.getPassword());
+        User user = signUpRequest.to(signUpRequest.getNickName(), signUpRequest.getEmail(), password);
+        userRepository.save(user);
     }
 }
