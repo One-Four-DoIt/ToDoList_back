6f6636371979f82a66ba34000c8b78d4ebb57e3d
joon <khjoon372@nvaer.com>
2023-06-04 19:29:25 +0900

    feat:todo

--- src/main/java/com/toDoList/dto/responseMessage/ToDoMessage.java
+++ src/main/java/com/toDoList/dto/responseMessage/ToDoMessage.java
@@ -10,7 +10,13 @@ public class ToDoMessage {
     public enum SuccessMessage{
 
         SAVE_TODO_SUCCESS("ToDo 저장 성공했습니다."),
-        SELECT_ORDER_SUCCESS("ToDo 우선순위 조회에 성공했습니다.");
+        SELECT_ORDER_SUCCESS("ToDo 우선순위 조회에 성공했습니다."),
+        DELETE_TODO_SUCCESS("ToDo 삭제 성공했습니다."),
+        CHECK_TODO_SUCCESS("ToDo 체크 성공했습니다."),
+        UNCHECK_TODO_SUCCESS("ToDo 체크 해제 성공했습니다."),
+        UPDATE_TODO_SUCCESS("ToDo 수정 성공했습니다. ");
+
+
         private final String message;
     }
 }
