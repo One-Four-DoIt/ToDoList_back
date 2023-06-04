13696b9d1a1344f2bec660c7c839ee79eeeef591
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-06-05 01:51:10 +0900

    feat: Task 관련 예외 처리 구현

--- src/main/java/com/toDoList/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/toDoList/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.toDoList.global.exception;
 
+import com.toDoList.exception.TaskException.NoSuchTaskIdxException;
+import com.toDoList.exception.ToDoException.NoSuchToDoIdxException;
+import com.toDoList.exception.UserAuthException.NoSuchEmailException;
 import com.toDoList.global.dto.ErrorResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
+
+    @ExceptionHandler(NoSuchToDoIdxException.class)
+    public ResponseEntity<ErrorResponseDto> handleNoSuchToDoIdxException(NoSuchToDoIdxException e) {
+        log.error(LOG_FORMAT, e.getClass(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity.ok(ErrorResponseDto.create(e.getErrorCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NoSuchTaskIdxException.class)
+    public ResponseEntity<ErrorResponseDto> handleNoSuchTaskIdxException(NoSuchTaskIdxException e) {
+        log.error(LOG_FORMAT, e.getClass(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity.ok(ErrorResponseDto.create(e.getErrorCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NoSuchEmailException.class)
+    public ResponseEntity<ErrorResponseDto> handleNoSuchEmailException(NoSuchEmailException e) {
+        log.error(LOG_FORMAT, e.getClass(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity.ok(ErrorResponseDto.create(e.getErrorCode(), e.getMessage()));
+    }
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorResponseDto> handleApplicationException(ApplicationException e) {
