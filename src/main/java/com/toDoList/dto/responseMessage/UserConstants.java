dd9a724a08ae1690ca55790103642a067f190b7d
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-06-06 22:23:19 +0900

    feat: Token 예외처리 추가

--- src/main/java/com/toDoList/dto/responseMessage/UserConstants.java
+++ src/main/java/com/toDoList/dto/responseMessage/UserConstants.java
@@ -10,6 +10,7 @@ public class UserConstants {
         SIGNUP_SUCCESS("회원가입에 성공했습니다."),
         LOGIN_SUCCESS("로그인에 성공했습니다."),
         LOGOUT_SUCCESS("로그아웃에 성공했습니다."),
+        REISSUE_SUCCESS("재발급에 성공했습니다."),
         EDIT_PASSWORD_SUCCESS("패스워드 재설정에 성공했습니다."),
         EMAIL_SEND_SUCCESS("EMAIL 인증번호 전송에 성공했습니다."),
         EMAIL_DUP_SUCCESS("EMAIL 중복체크에 성공했습니다.");
@@ -19,7 +20,8 @@ public class UserConstants {
     @Getter
     @RequiredArgsConstructor
     public enum FailMessage {
-        UNVALID_EMAIL_MESSAGE("Email이 유효하지 않습니다.");
+        UNVALID_EMAIL_MESSAGE("Email이 유효하지 않습니다."),
+        UNVALID_TOKEN_MESSAGE("TOKEN이 유효하지 않습니다.");
 
         private final String message;
     }
@@ -27,7 +29,8 @@ public class UserConstants {
     @Getter
     @RequiredArgsConstructor
     public enum FailCode {
-        UNVALID_EMAIL_CODE("USR001");
+        UNVALID_EMAIL_CODE("USR001"),
+        UNVALID_TOKEN_CODE("USR002");
         private final String code;
     }
 }
